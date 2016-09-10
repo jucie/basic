11221bc89dfa50942747541757b8f1b386b139c2
JuciÊ Dias Andrade <ojucie@gmail.com>
2016-09-10 15:40:34 -0300

    Is able to handle a file list to easy testing.

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
 import (
+	"bufio"
 	"os"
 )
+
+func compileFromList(path string) {
+	f, err := os.Open(path)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	sc := bufio.NewScanner()
+	for sc.Scan() {
+		path = sc.Text()
+		compile(path)
+	}
+}
+
+func compile(path string) {
+	if len(path) < 1 {
+		return
+	}
+	if path[0] == '@' {
+		compileFromList(path[1:])
+		return
+	}
+	prog := loadProgram(path)
+	prog.generate()
+}
 
 func main() {
 	if len(os.Args) < 2 {
@@ -10,8 +37,6 @@ func main() {
 	}
 	for i := 1; i < len(os.Args); i++ {
 		path := os.Args[i]
-		prog := NewProgram()
-		prog.load(path)
-		prog.generate()
+		compile(path)
 	}
 }
