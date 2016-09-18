0dd6e18f45be178947098d0a37bada5e19858556
JuciÊ Dias Andrade <ojucie@gmail.com>
2016-09-18 09:34:11 -0300

    Parsing function definition and function call.

--- astFnCall.go
+++ astFnCall.go
@@ -9,12 +9,28 @@ func (p *parser) parseFnCall() *astFnCall {
 	l := p.lex.peek()
 
 	if l.token != tokId {
+		p.unexpected()
 		return nil
 	}
 	fn := l.s
+	p.lex.next()
+
+	if l.token != '(' {
+		p.unexpected()
+		return nil
+	}
+	p.lex.next()
+
 	arg := p.parseExpr()
 	if arg == nil {
 		return nil
 	}
+
+	if l.token != ')' {
+		p.unexpected()
+		return nil
+	}
+	p.lex.next()
+
 	return &astFnCall{fn, arg}
 }
--- astFunc.go
+++ astFunc.go
@@ -1,8 +1,51 @@
 package main
 
 type astFunc struct {
-	coord
-	id   rune
-	arg  rune
+	id   string
+	arg  string
 	expr *astExpr
+}
+
+func (p *parser) parseFunc() *astFunc {
+	result := &astFunc{}
+	l := p.lex.peek()
+
+	if l.token != tokId {
+		p.unexpected()
+		return nil
+	}
+	result.id = l.s
+	p.lex.next()
+
+	if l.token != '(' {
+		p.unexpected()
+		return nil
+	}
+	p.lex.next()
+
+	if l.token != tokId {
+		p.unexpected()
+		return nil
+	}
+	result.arg = l.s
+	p.lex.next()
+
+	if l.token != ')' {
+		p.unexpected()
+		return nil
+	}
+	p.lex.next()
+
+	if l.token != '=' {
+		p.unexpected()
+		return nil
+	}
+	p.lex.next()
+
+	result.expr = p.parseExpr()
+	if result.expr == nil {
+		return nil
+	}
+
+	return result
 }
