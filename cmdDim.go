58e96f97c60ec0cfc215a44ccc05a393200259d2
JuciÊ Dias Andrade <ojucie@gmail.com>
2016-09-19 19:33:05 -0300

    Parsing command DIM.

--- cmdDim.go
+++ cmdDim.go
@@ -1,9 +1,20 @@
 package main
 
 type cmdDim struct {
+	vars []*astVarRef
 }
 
 func (p *parser) parseDim() *cmdDim {
-	p.consumeCmd()
-	return &cmdDim{}
+	result := &cmdDim{}
+	l := p.lex.peek()
+
+	for {
+		v := p.parseVarRef()
+		result.vars = append(result.vars, v)
+		if l.token != ',' {
+			break
+		}
+		p.lex.next()
+	}
+	return result
 }
