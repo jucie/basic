4a0e5c5e543fa4af277977ec4ff1d921015e4079
Jucie Andrade <jucie.andrade@yandeh.com.br>
2016-09-20 08:24:11 -0300

    Command DEF parsing.

--- cmdDef.go
+++ cmdDef.go
@@ -1,9 +1,48 @@
 package main
 
 type cmdDef struct {
+	id   string
+	arg  string
+	expr *astExpr
 }
 
 func (p *parser) parseDef() *cmdDef {
-	p.consumeCmd()
-	return &cmdDef{}
+	l := p.lex.peek()
+	result := &cmdDef{}
+
+	if l.token != tokFn {
+		return nil
+	}
+	p.lex.next()
+
+	if l.token != tokId {
+		return nil
+	}
+	result.id = l.s
+	p.lex.next()
+
+	if l.token != '(' {
+		return nil
+	}
+	p.lex.next()
+
+	if l.token != tokId {
+		return nil
+	}
+	result.arg = l.s
+	p.lex.next()
+
+	if l.token != ')' {
+		return nil
+	}
+	p.lex.next()
+
+	if l.token != '=' {
+		return nil
+	}
+	p.lex.next()
+
+	result.expr = p.parseExpr()
+
+	return result
 }
