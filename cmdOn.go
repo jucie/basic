ee86153c9f00bcdbc26962bc3f864d471269d55f
Jucie Andrade <jucie.andrade@yandeh.com.br>
2016-09-20 09:32:41 -0300

    Command ON parsing.

--- cmdOn.go
+++ cmdOn.go
@@ -1,10 +1,39 @@
 package main
 
+import (
+	"strconv"
+)
+
 type cmdOn struct {
-	dst int
+	expr *astExpr
+	dsts []int
 }
 
 func (p *parser) parseOn() *cmdOn {
-	p.consumeCmd()
-	return &cmdOn{}
+	result := &cmdOn{}
+	l := p.lex.peek()
+
+	result.expr = p.parseExpr()
+	if result.expr == nil {
+		return nil
+	}
+
+	if l.token != tokGoto {
+		return nil
+	}
+	p.lex.next()
+
+	for l.token == tokNumber {
+		dst, err := strconv.Atoi(l.s)
+		if err != nil {
+			return nil
+		}
+		result.dsts = append(result.dsts, dst)
+		p.lex.next()
+		if l.token != ',' {
+			break
+		}
+		p.lex.next()
+	}
+	return result
 }
