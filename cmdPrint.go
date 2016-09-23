6e48cc754f5e8689189e680657a2e8dae9f35c1c
Jucie Andrade <jucie.andrade@yandeh.com.br>
2016-09-23 11:55:00 -0300

    Command PRINT parsing.

--- cmdPrint.go
+++ cmdPrint.go
@@ -1,9 +1,39 @@
 package main
 
+type subCmd interface {
+}
+
 type cmdPrint struct {
+	subCmds []subCmd
 }
 
 func (p *parser) parsePrint() *cmdPrint {
-	p.consumeCmd()
-	return &cmdPrint{}
+	result := &cmdPrint{}
+	l := p.lex.peek()
+
+Loop:
+	for {
+		var subCmd subCmd
+		switch l.token {
+		case ';':
+			fallthrough
+		case ',':
+			subCmd = l.token
+			p.lex.next()
+		case tokEol:
+			fallthrough
+		case tokEof:
+			fallthrough
+		case ':':
+			break Loop
+		default:
+			expr := p.parseExpr()
+			if expr == nil {
+				break Loop
+			}
+			subCmd = expr
+		}
+		result.subCmds = append(result.subCmds, subCmd)
+	}
+	return result
 }
