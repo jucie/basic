8fcf13397c26a0b2b8544a6cdcebf7ff01fd0ad1
Jucie Andrade <jucie.andrade@yandeh.com.br>
2016-09-23 11:57:58 -0300

    Command INPUT parsing.

--- cmdInput.go
+++ cmdInput.go
@@ -1,9 +1,36 @@
 package main
 
 type cmdInput struct {
+	subCmds []subCmd
 }
 
 func (p *parser) parseInput() *cmdInput {
-	p.consumeCmd()
-	return &cmdInput{}
+	result := &cmdInput{}
+	l := p.lex.peek()
+
+Loop:
+	for {
+		var subCmd subCmd
+		switch l.token {
+		case ';':
+			fallthrough
+		case ',':
+			subCmd = l.token
+			p.lex.next()
+		case tokEol:
+			fallthrough
+		case tokEof:
+			fallthrough
+		case ':':
+			break Loop
+		default:
+			expr := p.parseExpr()
+			if expr == nil {
+				break Loop
+			}
+			subCmd = expr
+		}
+		result.subCmds = append(result.subCmds, subCmd)
+	}
+	return result
 }
