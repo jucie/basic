005a7d80668be62131011e7f532a16082198af00
Jucie Andrade <jucie.andrade@yandeh.com.br>
2016-09-23 10:53:31 -0300

    Command IF parsing.

--- cmdIf.go
+++ cmdIf.go
@@ -1,9 +1,37 @@
 package main
 
+import (
+	"strconv"
+)
+
 type cmdIf struct {
+	expr *astExpr
+	cmds []cmd
 }
 
 func (p *parser) parseIf() *cmdIf {
-	p.consumeCmd()
-	return &cmdIf{}
+	result := &cmdIf{}
+	l := p.lex.peek()
+
+	result.expr = p.parseExpr()
+	if result.expr == nil {
+		return nil
+	}
+
+	if l.token != tokThen {
+		return nil
+	}
+	p.lex.next()
+
+	if l.token == tokNumber {
+		line, err := strconv.Atoi(l.s)
+		if err != nil {
+			return nil
+		}
+		result.cmds = append(result.cmds, cmdGoto{line})
+	} else {
+		result.cmds = p.parseLineTail()
+	}
+
+	return result
 }
