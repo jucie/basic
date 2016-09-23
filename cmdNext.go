b9bf3c8d0c535bb2740cbf8559f1ee6f08d889ae
Jucie Andrade <jucie.andrade@yandeh.com.br>
2016-09-23 11:20:26 -0300

    Command NEXT parsing.

--- cmdNext.go
+++ cmdNext.go
@@ -1,9 +1,23 @@
 package main
 
 type cmdNext struct {
+	ids []string
 }
 
 func (p *parser) parseNext() *cmdNext {
-	p.consumeCmd()
-	return &cmdNext{}
+	result := &cmdNext{}
+	l := p.lex.peek()
+
+	for l.token == tokId {
+		result.ids = append(result.ids, l.s)
+		p.lex.next()
+		if l.token == ',' {
+			p.lex.next()
+		}
+	}
+	if len(result.ids) == 0 {
+		return nil
+	}
+
+	return result
 }
