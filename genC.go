a52fc9c39d2f959be7723f834d95e3159b6747f7
Jucie Dias Andrade <ojucie@gmail.com>
2019-10-12 19:14:21 -0300

    driver for C language code generator

---
+++ genC.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bufio"
+)
+
+type generatorForC struct {
+}
+
+func newGeneratorForC() *generatorForC {
+	return &generatorForC{}
+}
+
+func (gen *generatorForC) generate(wr *bufio.Writer, prog *program) {
+	prog.blocks.generateC(wr)
+}
