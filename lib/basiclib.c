1341b021ba1840c2b924915339e3327c9751b752
Jucie Dias Andrade <ojucie@gmail.com>
2019-10-17 19:43:00 -0300

    Support library

---
+++ lib/basiclib.c
@@ -0,0 +1,477 @@
+#include "basiclib.h"
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+
+static void ops(const char *msg){
+    fprintf(stderr, "\n%s\n", msg);
+    abort();
+}
+
+static void dim(arr *a, size_t elem_size, int argcnt, va_list list){
+    va_list ap;
+    size_t total;
+    int i;
+    size_t *p;
+
+    ap = list;
+    total = elem_size;
+    for (i = 0; i < argcnt; i++){
+	size_t size = (size_t)va_arg(ap, num);
+	if (size < 1) {
+	    ops("DIMension must be greater than zero");
+	}
+	total *= size +1;
+    }
+    total += argcnt * sizeof(size_t);
+    
+    p = *a;
+    p = realloc(p, total);
+    if (!p){
+	ops("Out of memory");
+    }
+    memset(p, 0, total);
+    *a = p;
+
+    ap = list;
+    for (i = 0; i < argcnt; i++){
+	p[i] = ((size_t)va_arg(ap, num)) +1;
+    }
+}
+
+void dim_num(arr *a, int argcnt, ...){
+    va_list ap;
+    va_start(ap, argcnt);
+    dim(a, sizeof(num), argcnt, ap);
+    va_end(ap);
+}
+
+void dim_str(arr *a, int argcnt, ...){
+    va_list ap;
+    va_start(ap, argcnt);
+    dim(a, sizeof(str), argcnt, ap);
+    va_end(ap);
+}
+
+
+static void *element_in_array(arr *a, size_t elem_size, int argcnt, va_list ap){
+    size_t *p;
+    size_t offset, multiplier;
+    int i;
+
+    if (*a == NULL) { /* Was this array DIMensioned yet? No? OK then. 8^) */
+	dim_num(a, 1, 10); /* As weird as it seems, it's a BASIC thing. */
+	/* An array may be used without being DIMensioned beforehand. */
+	/* In this case, the "default" dimension is 1x10 (really 11). */
+    }
+    p = *a;
+
+    offset = 0;
+    multiplier = 1;
+    for (i = 0; i < argcnt; i++, p++){
+	size_t pos = (size_t)va_arg(ap, num);
+	if (pos >= *p) {
+	    ops("Index out of bounds");
+	}
+	offset += multiplier * pos;
+	multiplier *= *p;
+    }
+
+    return ((char*)p)+(offset * elem_size);
+}
+
+num *num_in_array(arr *a, int argcnt, ...){
+    num *result;
+    va_list ap;
+    va_start(ap, argcnt);
+    result = (num*)element_in_array(a, sizeof(num), argcnt, ap);
+    va_end(ap);
+    return result;
+}
+
+str *str_in_array(arr *a, int argcnt, ...){
+    str *result;
+    va_list ap;
+    va_start(ap, argcnt);
+    result = (str*)element_in_array(a, sizeof(str), argcnt, ap);
+    va_end(ap);
+    return result;
+}
+
+void let_num(num *dst, num src){
+    *dst = src;
+}
+
+void let_str(str *dst, str src){
+    size_t size = strlen(src)+1;
+    *dst = realloc(*dst, size);
+    strcpy(*dst, src);
+}
+
+#define LOOP_TABLE_SIZE 64
+
+typedef struct {
+    num *var;
+    num start;
+    num end;
+    num step;
+    int target;
+} loop_ctrl;
+loop_ctrl tab_loop_ctrl[LOOP_TABLE_SIZE], *current_loop;
+
+void for_num(num *var, num start, num end, num step, int target){
+    if (!current_loop){
+	current_loop = tab_loop_ctrl;
+    } else {
+	if (current_loop - tab_loop_ctrl >= LOOP_TABLE_SIZE) {
+	    ops("Too many nested FOR...NEXT loops");
+	}
+	current_loop++;
+    }
+    current_loop->var = var;
+    current_loop->start = start;
+    current_loop->end = end;
+    current_loop->step = step;
+    current_loop->target = target;
+    *var = start;
+}
+
+int next(int *target){
+    if (!current_loop){
+	ops("NEXT without FOR");
+    } 
+
+    *current_loop->var += current_loop->step;
+
+    if (current_loop->start < current_loop->end && *current_loop->var <= current_loop->end){
+	*target = current_loop->target;
+	return 1;
+    }
+    if (current_loop->start > current_loop->end && *current_loop->var >= current_loop->end){
+	*target = current_loop->target;
+	return 1;
+    }
+
+    if (current_loop == tab_loop_ctrl) {
+	current_loop = NULL;
+    } else {
+	current_loop--;
+    }
+    return 0;
+}
+    
+
+static char input_buffer[4 * 1024], *input_ptr;
+
+void input_to_buffer(){
+    char *p;
+
+    print_str("? ");
+    if (!fgets(input_buffer, sizeof input_buffer, stdin)){
+	ops("INPUT failed");
+    }
+
+    p = strchr(input_buffer, '\n');
+    if (p) {
+	*p = '\0';
+    }
+    input_ptr = input_buffer;
+}
+
+int input_num(num *dst){
+    char *p;
+    if (!input_ptr || *input_ptr == '\0'){
+	fprintf(stderr, "\nMissing number in input.\n");
+	*dst = 0;
+	return 0;
+    }
+
+    *dst = atof(input_ptr);
+    p = strchr(input_ptr, ',');
+    if (p) {
+	input_ptr = p +1;
+    } else {
+	input_ptr = NULL;
+    }
+    return 1;
+}
+
+int input_str(str *dst){
+    char *p;
+    size_t size;
+
+    if (!input_ptr || *input_ptr == '\0'){
+	fprintf(stderr, "\nMissing string in input.\n");
+	return 0;
+    }
+
+    p = strchr(input_ptr, ',');
+    if (p){
+	size = p - input_ptr;
+	*dst = realloc(*dst, size +1);
+	memcpy(*dst, input_ptr, size);
+	*dst[size] = '\0';
+	input_ptr = p+1;
+    } else {
+	size = strlen(input_ptr);
+	*dst = realloc(*dst, size +1);
+	strcpy(*dst, input_ptr);
+	input_ptr = NULL; /* input is depleted */
+    }
+    return 1;
+}
+
+static int current_column;
+
+void print_char(char c){
+    switch (c) {
+	case '\n':
+		current_column = 0;
+		break;
+	case '\t':
+		{
+		    int pos = ((current_column / 8) + 1) * 8;
+		    while (current_column < pos) {
+			print_char(' ');
+		    }
+		}
+		return;
+    }
+    if (c == '\n'){
+	current_column = 0;
+    } else {
+	current_column++;
+    }
+    putchar(c);
+}
+
+void print_num(num val){
+    char buffer[64], *p = buffer;
+    if (val == (int)val) {
+	sprintf(buffer, " %d ", (int)val);
+    } else {
+	sprintf(buffer, " %f ", val);
+    }
+    while (*p) {
+	print_char(*p++);
+    }
+}
+
+void print_str(str val){
+    if (!val) {
+	return;
+    }
+    for (;*val;val++) {
+	print_char(*val);
+    }
+}
+
+void TAB_void(num val){
+    int column = (int)val;
+    if (column <= current_column){
+	print_char('\n');
+    }
+    while (current_column < column) {
+	print_char(' ');
+    }
+}
+
+#define ADDRESS_STACK_SIZE 64
+static int address_stack[ADDRESS_STACK_SIZE], *address_stack_ptr;
+
+void push_address(int address){
+    if (!address_stack_ptr) {
+	address_stack_ptr = address_stack;
+    } else {
+	if (address_stack_ptr - address_stack >= ADDRESS_STACK_SIZE){
+	    ops("Too many nested GOSUBs");
+	}
+	address_stack_ptr++;
+    }
+    *address_stack_ptr = address;
+}
+
+int pop_address(int *address){
+    if (!address_stack_ptr || address_stack_ptr == address_stack) {
+	ops("RETURN without GOSUB");
+    }
+    *address = *address_stack_ptr--;
+    if (address_stack_ptr == address_stack){
+	address_stack_ptr = NULL;
+    }
+}
+
+static int data_area_for_num_index;
+
+void read_num(num *val){
+    extern const size_t data_area_for_num_cnt;
+    extern const num data_area_for_num[];
+
+    if (data_area_for_num_index >= data_area_for_num_cnt) {
+	ops("READ number past DATA");
+    }
+    *val = data_area_for_num[data_area_for_num_index++];
+}
+
+static int data_area_for_str_index;
+
+void read_str(str *val){
+    extern const size_t data_area_for_str_cnt;
+    extern const str data_area_for_str[];
+
+    if (data_area_for_str_index >= data_area_for_str_cnt) {
+	ops("READ string past DATA");
+    }
+    *val = data_area_for_str[data_area_for_str_index++];
+}
+
+void restore(void){
+    data_area_for_num_index = 0;
+    data_area_for_str_index = 0;
+}
+
+num ABS_num(num val){
+    return (num)fabs(val);
+}
+
+num ASC_num(str val){
+    if (!val){
+	ops("Trying to perform ASC function on an empty string");
+    }
+    return *val;
+}
+
+num ATN_num(num val){
+    return (num)atan(val);
+}
+
+str CHR_str(str *dst, num val){
+    str s = *dst = realloc(*dst, 2);
+    s[0] = (unsigned char)val;
+    s[1] = '\0';
+}
+
+num COS_num(num val){
+    return (num)cos(val);
+}
+
+num EXP_num(num val){
+    return (num)exp(val);
+}
+
+num INT_num(num val){
+    return (num)(int)val;
+}
+
+num LEN_num(str val){
+    if (!val){
+	return 0;
+    }
+    return strlen(val);
+}
+
+num LOG_num(num val){
+    return (num)log(val);
+}
+
+str RIGHT_str(str *dst, str s, num length){
+    size_t size;
+
+    if (!s){
+	free(*dst);
+	return *dst = NULL;
+    }
+
+    size = strlen(s);
+    if (size <= length) {
+	length = size;
+    }
+
+    if (length == 0){
+	free(*dst);
+	return *dst = NULL;
+    }
+
+    *dst = realloc(*dst, length +1);
+    memcpy(*dst, s +size -length, length);
+    *dst[length] = '\0';
+    return *dst;
+}
+
+str LEFT_str(str *dst, str s, num length){
+    size_t size;
+
+    if (!s){
+	free(*dst);
+	return *dst = NULL;
+    }
+
+    size = strlen(s);
+    if (size <= length) {
+	length = size;
+    }
+
+    if (length == 0){
+	free(*dst);
+	return *dst = NULL;
+    }
+
+    *dst = realloc(*dst, length +1);
+    memcpy(*dst, s, length);
+    *dst[length] = '\0';
+    return *dst;
+}
+
+str MID_str(str *dst, str s, num start, num length){
+    size_t size;
+
+    if (!s){
+	free(*dst);
+	return *dst = NULL;
+    }
+
+    start--;
+    size = strlen(s);
+    if (size <= start) {
+	free(*dst);
+	return *dst = NULL;
+    }
+    return LEFT_str(dst, s + start, length);
+}
+
+num RND_num(num val){
+    val = val;
+    /* see https://stackoverflow.com/questions/13408990/how-to-generate-random-float-number-in-c#13409133 */
+    return ((float)rand()/(float)(RAND_MAX));
+}
+
+num SGN_num(num val){
+    return signbit(val);
+}
+
+num SIN_num(num val){
+    return (num)sin(val);
+}
+
+num SQR_num(num val){
+    return (num)sqrt(val);
+}
+
+str STR_str(str *dst, num val){
+    char buffer[64];
+    size_t size = sprintf(buffer, "%f", val);
+    *dst = realloc(*dst, size +1);
+    memcpy(*dst, buffer, size);
+    *dst[size] = '\0';
+}
+
+num TAN_num(num val){
+    return (num)tan(val);
+}
+
+num VAL_num(str val){
+    return (num)atof(val);
+}
+
---
+++ lib/basiclib.h
@@ -0,0 +1,60 @@
+#ifndef BASICLIB_H
+#define BASICLIB_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef float num;
+typedef unsigned char* str;
+typedef size_t* arr;
+
+void dim_num(arr *a, int argcnt, ...);
+void dim_str(arr *a, int argcnt, ...);
+
+num *num_in_array(arr *a, int argcnt, ...);
+str *str_in_array(arr *a, int argcnt, ...);
+
+void let_num(num *dst, num src);
+void let_str(str *dst, str src);
+
+void for_num(num *var, num start, num end, num step, int target);
+int next(int *target);
+
+void input_to_buffer();
+int input_num(num *dst);
+int input_str(str *dst);
+
+void print_num(num val);
+void print_str(str val);
+void print_char(char c);
+
+void push_address(int address);
+int pop_address(int *address);
+
+void read_num(num *val);
+void read_str(str *val);
+void restore(void);
+
+num ABS_num(num val);
+num ASC_num(str val);
+num ATN_num(num val);
+str CHR_str(str *dst, num val);
+num COS_num(num val);
+num EXP_num(num val);
+num INT_num(num val);
+str LEFT_str(str *dst, str s, num length);
+num LEN_num(str val);
+num LOG_num(num val);
+str MID_str(str *dst, str s, num start, num length);
+str RIGHT_str(str *dst, str s, num length);
+num RND_num(num val);
+num SGN_num(num val);
+num SIN_num(num val);
+num SQR_num(num val);
+str STR_str(str *dst, num val);
+void TAB_void(num val);
+num TAN_num(num val);
+num VAL_num(str val);
+
+#endif /* BASICLIB_H */
